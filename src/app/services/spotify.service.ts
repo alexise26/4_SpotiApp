a0146f9b4278c47d319d8cbe7924bee873f8b885
alexise26 <alexise26@gmail.com>
2017-10-09 21:31:37 -0600

    Artists added

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class SpotifyService {
+
+  artistas:any[]=[];
+  urlBusqueda:string="https://api.spotify.com/v1/search";
+  urlArtista:string="https://api.spotify.com/v1/artists";
+  token:string="Bearer BQDUOiBeVnGq8v7KEqNE_FvT23g39Ga4sTxaQ1UmbaHMJrwUC2oaCSCxsHmvVlvapFrlvq0Z7rA-O6hEzofdDjqzINzWzbV4Qe0R_DgM4RumU_H8hs8bFExyplBaMaUVXWFSS-S_-lMjzqIUXArp9yyd6Q";
+
+  constructor(private http:Http) { }
+
+  getArtistas(termino:string){
+     let headers = new Headers();
+     headers.append('Authorization', this.token);
+    let query=`?q=${termino}&type=artist`;
+    let url=this.urlBusqueda + query;
+
+    return this.http.get(url, {headers})
+      .map(res=>{
+        //console.log(res.json().artists.items);
+        this.artistas= res.json().artists.items;
+        //return res.json().artists.items;
+      })
+
+  }
+  getArtista(id:string){
+    let headers = new Headers();
+    headers.append('Authorization', this.token);
+   let query=`/${id}`;
+   let url=this.urlArtista + query;
+
+   return this.http.get(url, {headers})
+     .map(res=>{
+       console.log(res.json());
+       return res.json();
+     })
+  }
+  getTop(id:string){
+    let headers = new Headers();
+    headers.append('Authorization', this.token);
+   let query=`/${id}/top-tracks?country=US`;
+   let url=this.urlArtista + query;
+
+   return this.http.get(url, {headers})
+     .map(res=>{
+       console.log(res.json().tracks);
+       return res.json().tracks;
+     })
+  }
+
+}
